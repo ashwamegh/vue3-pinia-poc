0d104df774d229762c4b54afe2e188c56ab5a26d
ashwamegh <shashank7200@gmail.com>
2023-09-15 19:47:16 +0530

    added pinia store logic and components for page

--- src/App.vue
+++ src/App.vue
@@ -6,12 +6,26 @@
       <img src="./assets/pinia-logo.svg" alt="pinia-logo">
       <h1>Pinia Tasks</h1>
     </header>
+
+    <div class="task-list">
+      <div v-for="task in taskStore.tasks" :key="task.id">
+        <TaskDetails :task="task"/>
+      </div>
+    </div>
     
   </main>
 </template>
 
 <script>
+import { useTaskStore } from './stores/TaskStore';
+import TaskDetails from './components/TaskDetails.vue'
+
   export default {
-    
+    components: {TaskDetails},
+    setup () {
+      return ({
+        taskStore: useTaskStore()
+      })
+    }
   }
 </script>
