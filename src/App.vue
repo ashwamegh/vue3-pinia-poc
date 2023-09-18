e97c890540e1d18bbf622c2f4e7bf342694717ac
shashank7200 <shashank7200@gmail.com>
2023-09-18 10:31:56 +0530

    updating store reset functionality and store to refs

--- src/App.vue
+++ src/App.vue
@@ -8,7 +8,7 @@
     </header>
 
     <!-- loading -->
-    <div class="loading" v-if="taskStore.loading">Loading tasks...</div>
+    <div class="loading" v-if="loading">Loading tasks...</div>
 
     <!-- new task -->
     <div class="new-task">
@@ -19,19 +19,20 @@
     <nav class="filter">
       <button @click="filter = 'all'">All tasks</button>
       <button @click="filter = 'favs'">Fav Tasks</button>
+      <button @click="taskStore.$reset">Reset Tasks</button>
     </nav>
 
     <!-- Task lists -->
     <div class="task-list" v-if="filter === 'all'">
-      <p>You have {{ taskStore.totalCount }} tasks left to do</p>
-      <div v-for="task in taskStore.tasks" :key="task.id">
+      <p>You have {{ totalCount }} tasks left to do</p>
+      <div v-for="task in tasks" :key="task.id">
         <TaskDetails :task="task" />
       </div>
     </div>
 
     <div class="task-list" v-if="filter === 'favs'">
-      <p>You have {{ taskStore.favCount }} tasks left to do</p>
-      <div v-for="task in taskStore.favs" :key="task.id">
+      <p>You have {{ favCount }} tasks left to do</p>
+      <div v-for="task in favs" :key="task.id">
         <TaskDetails :task="task" />
       </div>
     </div>
@@ -44,18 +45,25 @@
   import { useTaskStore } from './stores/TaskStore';
   import TaskDetails from './components/TaskDetails.vue'
   import TaskForm from './components/TaskForm.vue';
+import { storeToRefs } from 'pinia';
 
   export default {
     components: {TaskDetails, TaskForm},
     setup () {
       const taskStore = useTaskStore()
       const filter = ref("all")
+      const { tasks, favs, loading, favCount, totalCount } = storeToRefs(taskStore)
 
       // fetch tasks
       taskStore.getTasks()
       return ({
         taskStore,
-        filter
+        filter,
+        tasks,
+        favs,
+        loading,
+        favCount,
+        totalCount
       })
     }
   }
